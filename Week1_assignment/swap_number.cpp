8cde613e15de63c66eeb67400f4f1b8c42554507
SONU KUMAR <148686085+sonu-kumar9113@users.noreply.github.com>
2025-02-02 13:30:57 +0530

    Add files via upload

--- Week1_assignment/swap_number.cpp
+++ Week1_assignment/swap_number.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int a,b;
+    cout<<"Enter a : "<<endl;
+    cin>>a;
+    cout<<"Enter b : "<<endl;
+    cin>>b;
+    cout<<"Before swapping :"<<endl;
+    cout<<"a is "<<a<<endl;
+    cout<<"b is "<<b<<endl;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"After swapping :"<<endl;
+    cout<<"a is "<<a<<endl;
+    cout<<"b is "<<b<<endl;
+
+
+}
